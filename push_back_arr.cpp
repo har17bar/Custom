eea2deeb136d8d303abfa343bfc5296bb2e70a38
Harut <har17bar@gmail.com>
2018-06-04 19:27:17 +0400

    push_back array

---
+++ push_back_arr.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void pushback(int *&arr,int &size,const int value){
+     int* newarr=new int(size+1);
+     for(int i=0;i<size;i++){
+         newarr[i]=arr[i];
+     }
+     newarr[size]=value;
+     delete [] arr;
+     arr=newarr;
+     size++;
+}
+
+int main()
+{
+    int size=5;
+    int* arr=new int(size);
+    cout<<"last"<<endl;
+    for(int i=0;i<size;i++){
+        arr[i]=i+1;
+        cout<<arr[i]<<"\t";
+    }
+    pushback(arr,size,15);
+    cout<<endl<<"new"<<endl;
+    for(int j=0;j<size;j++){
+        cout<<arr[j]<<"\t";
+    }
+    delete [] arr;
+}
